cbfb91f54e25cecc807681acec54ca612b8a1273
sobiemir <sobiemir@aculo.pl>
2023-12-15 18:16:44 +0100

    Add handling conversion via controller.

---
+++ Bunting.Api/Convert/ConvertController.cs
@@ -0,0 +1,53 @@
+﻿using Bunting.Abstractions.Conversion;
+using Bunting.Abstractions.Media;
+using Bunting.Api.Convert.Run;
+using Bunting.Conversion.Conversion;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bunting.Api.Controllers
+{
+    [ApiController]
+    [Route("convert")]
+    public sealed class ConvertController
+    {
+        private readonly IConversionFactory _factory;
+
+        public ConvertController(IConversionFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [HttpGet]
+        public Task GetVersion()
+        {
+            return Task.CompletedTask;
+        }
+
+        [HttpGet("available")]
+        public Task GetAvailable()
+        {
+            return Task.CompletedTask;
+        }
+
+        [HttpPost]
+        public async Task RunAsync([FromForm] ConvertRunRequest request, CancellationToken cancellationToken)
+        {
+            var sourceMediaType = MediaFormat.Of(request.File.ContentType);
+            var targetMediaType = MediaFormat.Of(request.TargetMediaType);
+
+            var format = new ConversionFormat(sourceMediaType, targetMediaType);
+
+            using var stream = request.File.OpenReadStream();
+
+            var engine = await _factory.CreateEngineAsync(stream, format, request.Options, cancellationToken);
+
+            await engine.ConvertAsync(cancellationToken);
+        }
+
+        [HttpPost("valid")]
+        public Task IsTransformValid()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
